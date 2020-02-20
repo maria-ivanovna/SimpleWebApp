67ed16a7fa59c0af12a801462d7ad287ad8cc67a
maria-ivanovna <vladimir.grudovich@gmail.com>
2020-02-20 15:12:23 +0200

    new Branche com

---
+++ src/main/java/entities/DBUser.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class DBUser {
+    private int id;
+    private String name;
+    private String pass;
+
+    public DBUser(String name, String pass) {
+        this.name = name;
+        this.pass = pass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
